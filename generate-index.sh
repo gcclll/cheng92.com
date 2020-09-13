74a06c79882672340d980744914a42e1043cf72f
gcclll <gccll.love@gmail.com>
2020-09-13 11:39:04 +0800

    feat:.env

--- generate-index.sh
+++ generate-index.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
 
+echo "
 ALGOLIA_APP_ID=EDIKM3W4HV
 ALGOLIA_INDEX_NAME=cheng92.com
 ALGOLIA_INDEX_FILE=public/algolia.json
-ALGOLIA_ADMIN_KEY=
+ALGOLIA_ADMIN_KEY=${ALGOLIA_INDEX_FILE}" > .env
+
+npm install
+npm run algolia
