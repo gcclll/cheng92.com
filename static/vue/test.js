4cfb6f14b6e05434742b193dd90ff52d43b0c3b4
gcclll <gccll.love@gmail.com>
2020-09-23 15:08:43 +0800

    05

--- static/vue/test.js
+++ static/vue/test.js
@@ -1,7 +1,41 @@
-const Counter = {
-  data() {
-    return { counter: 0 };
-  },
-};
+(function anonymous() {
+  const _Vue = Vue;
+  const {
+    createVNode: _createVNode,
+    createCommentVNode: _createCommentVNode,
+    createTextVNode: _createTextVNode,
+  } = _Vue;
 
-Vue.createApp(Counter).mount("#v-counter");
+  const _hoisted_1 = { key: 0 };
+
+  return function render(_ctx, _cache) {
+    with (_ctx) {
+      const {
+        toDisplayString: _toDisplayString,
+        createVNode: _createVNode,
+        openBlock: _openBlock,
+        createBlock: _createBlock,
+        createCommentVNode: _createCommentVNode,
+        createTextVNode: _createTextVNode,
+      } = _Vue;
+
+      return (
+        _openBlock(),
+        _createBlock(
+          "div",
+          {
+            id: "foo",
+            class: bar.baz,
+          },
+          [
+            _createTextVNode(_toDisplayString(world.burn()), 1 /* TEXT */),
+            ok
+              ? (_openBlock(), _createBlock("div", _hoisted_1, "yes"))
+              : _createCommentVNode("v-if", true),
+          ],
+          2 /* CLASS */
+        )
+      );
+    }
+  };
+});
