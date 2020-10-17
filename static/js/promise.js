183ee351984467b9fea62061d41fed5043b9614e
gcclll <gccll.love@gmail.com>
2020-10-17 18:44:01 +0800

    2020-10-17 18:44:01

--- static/js/promise.js
+++ static/js/promise.js
@@ -3,9 +3,9 @@ var PENDING = 0;
 var FULFILL = 1;
 var REJECT = 2;
 var i = 0;
-var proto = MyPromise.prototype;
 var noop = function () {};
 
+let originalThen = then;
 function MyPromise(resolver) {
   this[PID] = i++;
   this._state = PENDING;
@@ -30,15 +30,167 @@ function MyPromise(resolver) {
     reject(this, e);
   }
 }
+function originalResolve(value) {
+  const ctor = this;
+
+  // resolve 结果就是一个 Promise 实例
+  if (typeof value === "object" && value.constructor === ctor) {
+    return value;
+  }
+
+  const ins = new ctor(noop);
+  resolve(ins, value);
+
+  return ins;
+}
+function originalReject(reason) {
+  const ctor = this;
+
+  if (typeof value === "object" && value.constructor === ctor) {
+    return value;
+  }
+
+  const ins = new ctor(noop);
+  reject(promise, reason);
+  return ins;
+}
+function originalFinally(callback) {
+  const promise = this;
+  const ctor = promise.constructor;
+  // 1. 保证 callback 总是执行，即相当于在最后又挂了个 then，
+  //   这样就能保证之前有多少 then 且这些 then 结果是 fulfilled 还是 rejected
+  //   这个都会被执行
+  // 2. 回调 callback 要被执行且要保证 callback 的执行结果
+  //   也能符合 promise then 链规则
+  if (typeof callback === "function") {
+    return promise.then(
+      (value) => ctor.resolve(callback()).then(() => value),
+      (reason) =>
+        ctor.resolve(callback()).then(() => {
+          throw reason;
+        })
+    );
+  }
+
+  return promise.then(callback, callback);
+}
+
 MyPromise.prototype.then = then;
+MyPromise.prototype.finally = originalFinally;
+MyPromise.prototype.catch = function (onRejection) {
+  // 因为如果有异常，异常会随着链式调用链中一直往后流，知道被处理掉
+  // 所以这里只要挂一个 then 去接受错误并处理就可以了
+  return this.then(null, onRejection);
+};
+MyPromise.resolve = originalResolve;
+MyPromise.reject = originalReject;
+MyPromise.race = function (entries) {
+  const ctor = this;
+
+  if (!Array.isArray(entries)) {
+    return new ctor((_, reject) =>
+      reject(new TypeError("race 参数必须是一个数组"))
+    );
+  } else {
+    return new ctor((resolve, reject) => {
+      // 遍历所有任务
+      const len = entries.length;
+      for (let i = 0; i < len; i++) {
+        // 直接调用 resolve 去执行任务，然后挂一个 then
+        // 因为 resolve 和 reject 只会被执行一次，所以一旦只要有个 entry
+        // 结束了就会执行后面的 then 去调用 resolve 或 reject，
+        // 后面的就算执行到了也 settled 了，也不会重复执行 resolve 和 reject
+        ctor.resolve(entries[i]).then(resolve, reject);
+      }
+    });
+  }
+};
 
 function resolve(promise, value) {
   if (promise === value) {
     reject(promise, Util.error.returnSelfPromise());
   } else if (Util.isObjectOrFunction(value)) {
-    // TODO
+    let then;
+
+    try {
+      then = value.then;
+    } catch (e) {
+      // value 可能是 undefined 或 null，或其他非法类型(如：数字)
+      reject(promise, e);
+      return;
+    }
+    handleMaybeThenable(promise, value, then);
   } else {
     fulfill(promise, value);
+  }
+}
+
+function handleForeignThenable(promise, thenable, then) {
+  asap(() =>
+    ((promise) => {
+      let sealed = false; // 保证只会执行一次
+      try {
+        then.call(
+          thenable,
+          (value) => {
+            if (sealed) return;
+            sealed = true;
+
+            resolve(promise, value);
+
+            // if (value !== thenable) {
+            //   resolve(promise, value);
+            // } else {
+            //   fulfill(promise, value);
+            // }
+          },
+          (reason) => {
+            if (sealed) return;
+            sealed = true;
+
+            reject(promise, reason);
+          }
+        );
+      } catch (e) {
+        sealed = true;
+        reject(promise, e);
+      }
+    })(promise)
+  );
+}
+
+function handleMaybeThenable(promise, thenable, then) {
+  // originalThen 实现的 then 函数，即 MyPromise.prototype.then
+  // 这能确保 thenable 的确是我们的 MyPromise 实例
+  if (
+    thenable.constructor === promise.constructor &&
+    // 因为 originalResolve 是直接挂到构造函数上的
+    thenable.constructor.resolve === originalResolve &&
+    thenable.then === originalThen
+  ) {
+    // 这里要做的处理是直接针对 thenable 状态做出判断
+    if (thenable._state === FULFILL) {
+      fulfill(promise, thenable._result);
+    } else if (thenable._state === REJECT) {
+      reject(promise, thenable._result);
+    } else {
+      // 直接构造 resolver 和 rejection
+      subscribe(
+        thenable,
+        undefined,
+        (val) => resolve(promise, val),
+        (reason) => reject(promise, reason)
+      );
+    }
+  } else {
+    if (then === undefined) {
+      fulfill(promise, thenable);
+    } else if (typeof then === "function") {
+      handleForeignThenable(promise, thenable, then);
+    } else {
+      // 普通类型对象
+      fulfill(promise, thenable);
+    }
   }
 }
 
@@ -202,7 +354,7 @@ function asap(callback) {
     // 因为 flush 是异步执行，所以在它还没之前之前有可能有新的任务入列
     // 这个时候 qlen > 1 ，直到 flush 执行，通过 qlen 遍 queue 确保在执行的时刻
     // 可以将这之前的所有入列的任务都得到执行
-    setTimeout(flush());
+    setTimeout(flush);
   }
 }
 
