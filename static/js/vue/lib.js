8a7bfbb0cb63d149810018d27d4d51acd485e579
gcclll <gccll.love@gmail.com>
2020-12-31 16:08:58 +0800

    feat: sfc

---
+++ static/js/vue/lib.js
@@ -0,0 +1,74 @@
+const {
+  compileScript,
+  parse,
+  compileStyle,
+  generate,
+  baseParse,
+  transform,
+  transformSrcset,
+  createSrcsetTransformWithOptions,
+  transformElement,
+  transformBind,
+  normalizeOptions,
+} = require(process.env.VNEXT_PKG_SFC + "/dist/compiler-sfc.cjs.js");
+const { log } = require(process.env.BLOG_JS + "/utils.js");
+
+const mockId = "xxxxxxxx";
+
+const compileSFC = (src, options = {}, fn) => {
+  const { descriptor } = parse(src);
+  fn && fn(descriptor);
+  const result = compileScript(descriptor, {
+    ...options,
+    id: mockId,
+    enableRefSugar: true,
+  });
+  return [result, descriptor];
+};
+
+function compileSFCScript(src, options) {
+  const { descriptor } = parse(src);
+  return compileScript(descriptor, {
+    ...options,
+    id: mockId,
+    enableRefSugar: true,
+  });
+}
+
+function compileWithSrcset(template, options) {
+  const ast = baseParse(template);
+  const srcsetTransform = options
+    ? createSrcsetTransformWithOptions(normalizeOptions(options))
+    : transformSrcset;
+
+  transform(ast, {
+    nodeTransforms: [srcsetTransform, transformElement],
+    directiveTransforms: {
+      bind: transformBind,
+    },
+  });
+  return generate(ast, { mode: "module" });
+}
+
+const src = `
+<img src="./logo.png" srcset="./logo.png"/>
+<img src="./logo.png" srcset="./logo.png 2x"/>
+<img src="./logo.png" srcset="./logo.png 2x"/>
+<img src="./logo.png" srcset="./logo.png, ./logo.png 2x"/>
+<img src="./logo.png" srcset="./logo.png 2x, ./logo.png"/>
+<img src="./logo.png" srcset="./logo.png 2x, ./logo.png 3x"/>
+<img src="./logo.png" srcset="./logo.png, ./logo.png 2x, ./logo.png 3x"/>
+<img src="/logo.png" srcset="/logo.png, /logo.png 2x"/>
+<img src="https://example.com/logo.png" srcset="https://example.com/logo.png, https://example.com/logo.png 2x"/>
+<img src="/logo.png" srcset="/logo.png, ./logo.png 2x"/>
+<img src="data:image/png;base64,i" srcset="data:image/png;base64,i 1x, data:image/png;base64,i 2x"/>`;
+
+module.exports = {
+  compileSFC,
+  log,
+  mockId,
+  compile: compileSFCScript,
+  compileStyle,
+  compileWithSrcset,
+  src,
+};
