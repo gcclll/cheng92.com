27ff56dabd1f3124c87a51f9d5f41c87e9034c1e
gcclll <gccll.love@gmail.com>
2021-05-07 11:13:02 +0800

    feat(add): react render with text node

--- static/js/react/didact.js
+++ static/js/react/didact.js
@@ -1,12 +1,26 @@
-console.log('\n')
+console.log("\n");
 const Didact = {
   createElement,
-  render
-}
+  render,
+};
 
 // ReactDOM.render
 function render(element, container) {
-  // TODO
+  // 1. 创建当前树根节点元素
+  const dom =
+    element.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+
+  const isProperty = (key) => key !== "children";
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => (dom[name] = element.props[name]));
+
+  // 2. 遍历所有的 children 创建子元素
+  element.props.children.forEach((child) => render(child, dom /*parent*/));
+
+  container.appendChild(dom);
 }
 
 // React.createElement
@@ -15,28 +29,29 @@ function createElement(type, props, ...children) {
     type,
     props: {
       ...props,
-      children: children.map(child =>
-        typeof child === 'object' ? child : createTextElement(child))
-    }
-  }
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      ),
+    },
+  };
 }
 
 function createTextElement(text) {
   return {
-    type: 'TEXT_ELEMENT',
+    type: "TEXT_ELEMENT",
     props: {
       nodeValue: text, // 类似 textContent 可以修改节点文本内容的属性
-      children: []
-    }
-  }
-
+      children: [],
+    },
+  };
 }
 
+export { Didact };
 
 try {
   if (module) {
     module.exports = Didact;
   }
 } catch (e) {
-  console.warn(e.message);
+  console.warn("module:" + e.message);
 }
